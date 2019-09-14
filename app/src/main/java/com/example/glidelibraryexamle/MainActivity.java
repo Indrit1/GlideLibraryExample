28e5033b5d5bf6579c4cbf2db0eae47e14db336a
Indrit Saveta <savetaindrit@gmail.com>
2019-09-14 22:23:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/glidelibraryexamle/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.glidelibraryexamle;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+public class MainActivity extends AppCompatActivity {
+   ImageView imageView;
+   ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progress_circular);
+        Glide.with(MainActivity.this)
+                .load("https://res.cloudinary.com/demo/image/upload/sample.jpg")
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                }).into(imageView);
+    }
+}
